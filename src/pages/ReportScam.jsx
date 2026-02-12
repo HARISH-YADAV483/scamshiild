a5712c3f9ccdae7059eaae5add4c84c5786e50bb
HARISH-YADAV483 <harish4nitkkr@gmail.com>
2026-02-12 18:28:47 +0530

    Update developer name in ReportScam footer

--- src/pages/ReportScam.jsx
+++ src/pages/ReportScam.jsx
@@ -507,7 +507,7 @@ export default function ReportScam() {
 
 
        <footer className="footer">
-        👨‍💻 Developed by <span>Harish & Tarun (Benignants)</span>
+        👨‍💻 Developed by <span>Harish Puhaniya</span>
       </footer>
     </div>
   );
